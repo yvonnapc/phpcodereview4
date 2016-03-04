f087fb0e6fc2072cd1a7c52c0a80299a63c7a171
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-04 15:06:27 -0800

    Add feature to add a Brand and a Store to twig pages

--- app/app.php
+++ app/app.php
@@ -21,6 +21,57 @@
      return $app['twig']->render('index.html.twig');
    });
 
+   $app->get("/stores", function() use($app){
+     $stores = Store::getAll();
+     return $app['twig']->render('stores.html.twig', array('stores' => $stores));
+   });
+
+   $app->post("/add_store", function() use($app){
+     $name = $_POST['name'];
+     $id = null;
+     $new_store = new Store($name, $id);
+     $new_store->save();
+     $stores = Store::getAll();
+     return $app['twig']->render('stores.html.twig', array('stores' => $stores));
+   });
+
+   $app->get("/store/{id}", function($id) use($app){
+     $store = Store::find($id);
+     $brands = $store->getBrands();
+     $all_brands = Brand::getAll();
+     return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $brands, 'all_brands' =>$all_brands));
+   });
+
+
+
+
+
+
+   $app->get("/brands", function() use($app){
+     $brands = Brand::getAll();
+     return $app['twig']->render('brands.html.twig', array('brands' => $brands));
+   });
+
+   $app->post("/add_brand", function() use($app){
+     $name = $_POST['name'];
+     $id = null;
+     $new_brand = new Brand($name, $id);
+     $new_brand->save();
+     $brands = Brand::getAll();
+     return $app['twig']->render('brands.html.twig', array('brands' => $brands));
+   });
+
+   $app->get("/brand/{id}", function($id) use ($app) {
+      $brand = Brand::find($id);
+      $stores = $brand->getStores();
+      $all_stores = Store::getAll();
+      return $app['twig']->render('brand.html.twig', array('brand' => $brand, 'stores' => $stores, 'all_stores' => $all_stores));
+     });
+
+
+
+   return $app;
+
 
 
 
