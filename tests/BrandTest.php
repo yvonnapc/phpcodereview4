479a18f8004161ede2d422276aa9b8a4e8cc1cee
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-04 09:25:12 -0800

    Add passing getName test for Brand Class

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -7,14 +7,23 @@
 
   require_once "src/Brand.php";
 
-	$server = "mysql:host=localhost;dbname=shoe_test";
+	$server = "mysql:host=localhost;dbname=shoes_test";
 	$username = "root";
 	$password = "root";
 	$DB = new PDO($server, $username, $password);
 
   class BrandTest extends PHPUnit_Framework_TestCase
   {
-
+   function test_getName()
+   {
+     //Arrange
+     $name = "Clark's";
+     $test_brand = new Brand($name);
+     //Act
+     $result = $test_brand->getName();
+     //Assert
+     $this->assertEquals($name, $result);
+   }
 
 
 
