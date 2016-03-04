d0731100dd437aed686928b48acec88adab8f996
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-04 09:54:00 -0800

    Add save, getAll, and deleteAll methods for Brand Class

--- src/Brand.php
+++ src/Brand.php
@@ -22,6 +22,33 @@ class Brand
   {
     return $this->id;
   }
+  function save()
+  {
+      $GLOBALS['DB']->exec(
+      "INSERT INTO brands
+      (name)
+      VALUES
+      ('{$this->getName()}');"
+    );
+    $this->id = $GLOBALS['DB']->lastInsertId();
+  }
+  static function getAll()
+  {
+    $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
+    $brands = array();
+    foreach($returned_brands as $brand){
+      $name = $brand['name'];
+      $id = $brand['id'];
+      $new_brand = new Brand($name, $id);
+      array_push($brands, $new_brand);
+    }
+    return $brands;
+  }
+  static function deleteAll()
+  {
+    $GLOBALS['DB']->exec(
+    "DELETE FROM brands");
+  }
 
 }
 
--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -14,6 +14,10 @@
 
   class BrandTest extends PHPUnit_Framework_TestCase
   {
+    protected function tearDown()
+      {
+          Brand::deleteAll();
+      }
    function test_getName()
    {
      //Arrange
@@ -34,6 +38,34 @@
      $result = $test_brand->getId();
      //Assert
      $this->assertEquals(true, is_numeric($result));
+   }
+   function test_save()
+   {
+     //Arrange
+     $name = "Clark's";
+     $id = 1;
+     $test_brand = new Brand($name, $id);
+     $test_brand->save();
+     //Act
+     $result = Brand::getAll();
+     //Assert
+     $this->assertEquals([$test_brand], $result);
+   }
+   function test_getAll()
+   {
+     //Arrange
+     $name = "Clark's";
+     $test_brand = new Brand($name);
+     $test_brand->save();
+
+     $name = "Doc Marten's";
+     $test_brand2 = new Brand($name);
+     $test_brand2->save();
+     //Act
+     $result = Brand::getAll();
+
+     //Assert
+     $this->assertEquals([$test_brand, $test_brand2], $result);
    }
 
 
